0b0d8318329b8dcdf9a5b7bff8b6627810a5f272
shoom3301 <shoom3301@gmail.com>
2023-06-19 20:59:44 +0600

    chore: change url temporary

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 import { CowSwapWidgetEnv } from './types'
 
 export const COWSWAP_URLS: Record<CowSwapWidgetEnv, string> = {
-  local: 'http://localhost:3000',
+  local: 'https://swap-dev-git-feat-cow-widget-cowswap.vercel.app',
   prod: 'swap.cow.fi',
 }
